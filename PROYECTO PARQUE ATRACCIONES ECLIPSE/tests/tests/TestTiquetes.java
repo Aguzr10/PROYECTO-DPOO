ca80c02b5e6605dfa2020b174cd63982242752cc
juanesteban-matheo <je.lopeza1@uniandes.edu.co>
2025-05-10 21:03:45 -0500

    Progreso parcial de 'TestTiquetes.java'
    Todos los progresos parciales se refieren a la creación de atributos e instancias para las pruebas

--- PROYECTO PARQUE ATRACCIONES ECLIPSE/tests/tests/TestTiquetes.java
+++ PROYECTO PARQUE ATRACCIONES ECLIPSE/tests/tests/TestTiquetes.java
@@ -1,6 +1,33 @@
 package tests;
+import parque.Atracción.Atracciones;
+import parque.Categorías.Categoria;
 import parque.Tiquetes.*;
 
-public class TestTiquetes {
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestTiquetes {
+	// ATRIBUTOS E INSTANCIAS A USAR
+	private LocalDate fechaDisponibilidad, fechaInicio, fechaFinal;
+	private Categoria categoriaTiquete;
+	private boolean usoValidado, entradaParque;
+	private double precio;
+	private Atracciones atraccion;
+	
+	private FastPass fastPass;
+	private TiqueteBasico básico;
+	private TiqueteIndividual individual;
+	private TiqueteTemporada temporada;
+	
+	void setUp() {
+		fastPass = new FastPass(fechaDisponibilidad);
+		básico = new TiqueteBasico(categoriaTiquete, usoValidado, precio, entradaParque);
+		individual = new TiqueteIndividual(categoriaTiquete, usoValidado, precio, atraccion);
+		temporada = new TiqueteTemporada(categoriaTiquete, usoValidado, precio, fechaInicio, fechaFinal);
+	}
 }
