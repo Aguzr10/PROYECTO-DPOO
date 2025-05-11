50f6b0dc8ae14f2dbaac6e9436116781be0e7d59
juanesteban-matheo <je.lopeza1@uniandes.edu.co>
2025-05-10 19:11:32 -0500

    Progreso casi completo de 'TestCategorías.java'
    'Categorías.java' no aplica para pruebas directamente

--- PROYECTO PARQUE ATRACCIONES ECLIPSE/tests/tests/TestCategorías.java
+++ PROYECTO PARQUE ATRACCIONES ECLIPSE/tests/tests/TestCategorías.java
@@ -1,6 +1,24 @@
 package tests;
 import parque.Categorías.*;
 
-public class TestCategorías {
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestCategorías {
+	// ATRIBUTOS E INSTANCIAS A USAR
+	private LocalDate fechaInicio, fechaFinal;
+	private Temporada temporada;
+	
+	@BeforeAll
+	void setUp() {
+		temporada = new Temporada(fechaInicio, fechaFinal);
+	}
+	
+	@AfterAll
+	void tearDown() {}
 }
