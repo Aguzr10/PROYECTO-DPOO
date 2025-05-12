a75d7085a241a3f627cbbafbde710c429fc679b2
juanesteban-matheo <je.lopeza1@uniandes.edu.co>
2025-05-12 15:28:18 -0500

    'TestCategorías.java' completo
    'Categorías.java' no aplica para pruebas, sólo 'Temporada.java'

--- PROYECTO PARQUE ATRACCIONES ECLIPSE/tests/tests/TestCategorías.java
+++ PROYECTO PARQUE ATRACCIONES ECLIPSE/tests/tests/TestCategorías.java
@@ -11,12 +11,31 @@ import org.junit.jupiter.api.Test;
 
 public class TestCategorías {
 	// ATRIBUTOS E INSTANCIAS A USAR
-	private LocalDate fechaInicio, fechaFinal;
+	private LocalDate fechaInicio = LocalDate.of(2025, 1, 25),
+					  fechaIntermedia = LocalDate.of(2025, 3, 15),
+					  fechaFinal = LocalDate.of(2025, 5, 31),
+					  fechaPasada = LocalDate.of(2025, 6, 1);
 	private Temporada temporada;
 	
 	@BeforeAll
 	void setUp() {
 		temporada = new Temporada(fechaInicio, fechaFinal);
+	}
+	
+	@Test
+	void testSerializar() {
+		assertEquals("2025-1-25,2025-5-31", temporada.serializar(), "La serialización de la temporada no fue la esperada!");
+	}
+	
+	@Test
+	void testDuracionEnDias() {
+		assertEquals(128, temporada.duracionEnDias(), "El cálculo de días no fue el esperado!");
+	}
+	
+	@Test
+	void testFechaDentroDeRango() {
+		assertTrue(temporada.fechaDentroDeRango(fechaIntermedia), "La prueba de rango falló para una fecha válida!");
+		assertFalse(temporada.fechaDentroDeRango(fechaPasada), "La prueba de rango falló para una fecha inválida!");
 	}
 	
 	@AfterAll
